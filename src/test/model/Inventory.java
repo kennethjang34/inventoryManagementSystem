3803ce941ded4b793215540552d32cd1bc9a95bb
Junhyeok Jang <kennethjang34@gmail.com>
2021-10-13 16:47:44 -0700

    Draft version2 of inventory

---
+++ src/test/model/Inventory.java
@@ -0,0 +1,319 @@
+package model;
+
+
+import java.util.ArrayList;
+
+public class Inventory {
+    private final int codeSize;
+    private final ArrayList<Product> [] listByCode;
+    //A0 : [0], temporary storage room
+    //A1 : [1]
+    private final ArrayList<Product> [] listByLocation;
+
+    //this array list will represent a hashmap
+    //where key is a numeric product code and the value is an array of strings indicating its location in the inventory.
+
+    private final ArrayList<Integer> [] locationsOfProductCode;
+    private final int numberOfSections;
+    private static final int NUM_ALPHABETS = 26;
+    private int quantity;
+
+    //EFFECTS: create an empty inventory. By default, Item code and sections numbers will have three alphabets and
+    // 100 numbers from 0 to 99
+    public Inventory() {
+        codeSize = 3;
+        numberOfSections = 100;
+        listByCode = new ArrayList[(int)Math.pow(NUM_ALPHABETS, codeSize)];
+        locationsOfProductCode = new ArrayList[(int)Math.pow(NUM_ALPHABETS, codeSize)];
+        //In this case, the location code starts at A0 and ends at Z99
+        listByLocation = new ArrayList[NUM_ALPHABETS * numberOfSections];
+        for (int i = 0; i < listByCode.length; i++) {
+            listByCode[i] = new ArrayList<>();
+            locationsOfProductCode[i] = new ArrayList<>();
+        }
+        for (int i = 0; i < listByLocation.length; i++) {
+            listByLocation[i] = new ArrayList<>();
+        }
+        quantity = 0;
+    }
+
+    //REQUIRES: codeSize must be greater than 0;
+    //EFFECTS: create an empty inventory. Item code will have a number of alphabets specified by codeSize.
+    //By default, section number will range from 0 to 99 inclusive
+    public Inventory(int codeSize) {
+        this.codeSize = codeSize;
+        numberOfSections = 100;
+        listByCode = new ArrayList[(int)Math.pow(NUM_ALPHABETS, codeSize)];
+        locationsOfProductCode = new ArrayList[(int)Math.pow(NUM_ALPHABETS, codeSize)];
+        listByLocation = new ArrayList[NUM_ALPHABETS * numberOfSections];
+        for (int i = 0; i < listByCode.length; i++) {
+            listByCode[i] = new ArrayList<>();
+            locationsOfProductCode[i] = new ArrayList<>();
+        }
+        for (int i = 0; i < listByLocation.length; i++) {
+            listByLocation[i] = new ArrayList<>();
+        }
+        quantity = 0;
+    }
+
+    //REQUIRES: code size must be greater than 0, numberOfSections must be greater than 0.
+    //EFFECTS: create an empty inventory. Item code and section numbers will have
+    //a number of alphabets specified by codeSize and numbers range from 0 to numberOfSections-1
+    public Inventory(int codeSize, int numberOfSections) {
+        this.codeSize = codeSize;
+        this.numberOfSections = numberOfSections;
+        listByCode = new ArrayList[(int)Math.pow(NUM_ALPHABETS, codeSize)];
+        listByLocation = new ArrayList[NUM_ALPHABETS * numberOfSections];
+        locationsOfProductCode = new ArrayList[(int)Math.pow(NUM_ALPHABETS, codeSize)];
+        for (int i = 0; i < listByCode.length; i++) {
+            listByCode[i] = new ArrayList<>();
+            locationsOfProductCode[i] = new ArrayList<>();
+        }
+        for (int i = 0; i < listByLocation.length; i++) {
+            listByLocation[i] = new ArrayList<>();
+        }
+        quantity = 0;
+    }
+
+
+
+    //REQUIRES: products need to be a list of entries that contain Product, location in String.
+    public void addProducts(ArrayList<Object[]> products) {
+        for (Object[] productInfo: products) {
+            Product product = (Product)productInfo[0];
+            String location = (String)productInfo[1];
+            int itemCode = getItemCodeNumber(product.getItemCode());
+            int locationCode = getLocationCodeNumber(location);
+            listByCode[itemCode].add(product);
+            listByLocation[locationCode].add(product);
+            if (!locationsOfProductCode[itemCode].contains(locationCode)) {
+                locationsOfProductCode[itemCode].add(locationCode);
+            }
+        }
+        quantity += products.size();
+    }
+
+
+    //REQUIRES: product needs to exist in the inventory
+    //MODIFIES: this
+    //EFFECTS: remove the product indicated from the inventory. If the product is removed
+    //return true. If the product cannot be found, return false.
+    //implement try and catch block later (in case the products don't exist)
+    public boolean removeProduct(Product product) {
+        int code = getItemCodeNumber((product.getItemCode()));
+        if (listByCode[code].remove(product)) {
+            int locationCode = findLocation(product);
+            listByLocation[locationCode].remove(product);
+            return true;
+        }
+        return false;
+
+    }
+
+
+    //REQUIRES: products need to exist in the inventory
+    //MODIFIES: this
+    //EFFECTS: remove all the products indicated from the inventory
+    //implement try and catch block later (in case the products don't exist)
+    public void removeProducts(ArrayList<Product> products) {
+        for (Product e: products) {
+            int code = getItemCodeNumber((e.getItemCode()));
+            listByCode[code].remove(e);
+            //try and catch block needed
+            int locationCode = findLocation(e);
+            listByLocation[locationCode].remove(e);
+        }
+    }
+
+
+
+
+
+
+    @SuppressWarnings({"checkstyle:MethodLength", "checkstyle:SuppressWarnings"})
+    public boolean removeProducts(String itemCode, int qty) {
+        ArrayList<Product> listCode = listByCode[getItemCodeNumber(itemCode)];
+        if (listCode.size() == 0) {
+            return false;
+        }
+        ArrayList<Integer> locations = findLocations(itemCode);
+        int count = 0;
+        int index = 0;
+        int locationPos = 0;
+        ArrayList<Product> listLocation = listByLocation[locations.get(locationPos++)];
+        ArrayList<Product> toBeRemoved = new ArrayList<>();
+        while (count < qty) {
+            if (toBeRemoved.size() + count == qty) {
+                listCode.removeAll(toBeRemoved);
+                listLocation.removeAll(toBeRemoved);
+                break;
+            } else if (index == listLocation.size()) {
+                listCode.removeAll(toBeRemoved);
+                listLocation.removeAll(toBeRemoved);
+                count += toBeRemoved.size();
+                if (locationPos == locations.size()) {
+                    break;
+                }
+                index = 0;
+                listLocation = listByLocation[locations.get(locationPos++)];
+                toBeRemoved = new ArrayList<>();
+            }
+            Product product = listLocation.get(index);
+            if (product.getItemCode().equals(itemCode)) {
+                toBeRemoved.add(product);
+            }
+            index++;
+        }
+        return true;
+    }
+
+
+
+
+
+
+
+    //EFFECTS: return the numeric location code for the product.
+    //if the product is stored in the temporary storage room, return 0;
+    //If there is no such product in the inventory, return -1.
+    public int findLocation(Product product) {
+        ArrayList<Product> list;
+        ArrayList<Integer> locations = locationsOfProductCode[getItemCodeNumber(product.getItemCode())];
+        for (Integer e: locations) {
+            list = listByLocation[e];
+            if (list.contains(product)) {
+                return e;
+            }
+        }
+        return -1;
+    }
+
+    //numeric form of this item code must be existing in inventory.
+    //for example, if valid form of item code is "AAA", passed item code cannot be "QQQQ"
+    //EFFECTS: find the location of a product indicated by this item code and SKU
+    public int findLocation(String itemCode, int sku) {
+        for (Product e: listByCode[getItemCodeNumber(itemCode)]) {
+            if (e.getSku() == sku) {
+                return findLocation(e);
+            }
+        }
+        return -1;
+    }
+
+    //numeric form of this item code must be existing in inventory.
+    //Ex) if valid form of item code is "AAA", passed item code cannot be "AAAA"
+    //EFFECTS: return a list of locations where products belonging to the item code are located.
+    public ArrayList<Integer> findLocations(String itemCode) {
+        return locationsOfProductCode[getItemCodeNumber(itemCode)];
+    }
+
+
+    //REQUIRES: itemCode must be in valid form(a combination of english alphabets (upper case),
+    //of size used by the inventory
+    //EFFECTS: return a numeric code converted from the string code
+    public int getItemCodeNumber(String itemCode) {
+        int numericCode = 0;
+        itemCode = itemCode.toUpperCase();
+        for (int i = 0; i < itemCode.length(); i++) {
+            numericCode *= 26;
+            numericCode += itemCode.charAt(i) - 'A';
+        }
+        return numericCode;
+    }
+
+    //REQUIRES: location must be in the valid form(a combination of one english alphabet and digits,
+    //in the form used by the inventory (upper case))
+    //EFFECTS: return a numeric code converted from the string code. If location is null, return 0;
+    public int getLocationCodeNumber(String location) {
+        int numericCode;
+        if (location.equalsIgnoreCase("T")) {
+            return 0;
+        }
+        //alphabet's value
+        location = location.toUpperCase();
+        int alphabetValue = location.charAt(0) - 'A';
+        //If A99: 99, B99:
+        numericCode = alphabetValue * numberOfSections + Integer.parseInt(location.substring(1));
+        return numericCode;
+    }
+
+
+    //REQUIRE: numeric Code must be in a valid form
+    //EFFECTS: return string type item code
+    public String getStringItemCode(int numericCode) {
+        String stringCode = "";
+        stringCode += (char)((numericCode / (26 * 26)) + 'A');
+        numericCode = numericCode % (26 * 26);
+        stringCode += (char)(numericCode / 26 + 'A');
+        numericCode %= 26;
+        stringCode += (char)(numericCode + 'A');
+        return stringCode;
+    }
+
+    //REQUIRES: numeric Code must be in a valid form
+    //EFFECTS: return string type location code
+    public String getStringLocationCode(int numericCode) {
+        if (numericCode < 0) {
+            return null;
+        }
+        if (numericCode == 0) {
+            return "T";
+        }
+        String stringCode = "";
+        stringCode += (char)((numericCode / (numberOfSections)) + 'A');
+        numericCode %= numberOfSections;
+        stringCode += "" + numericCode;
+        return stringCode;
+    }
+
+
+    //EFFECTS: return the number of products belonging to the code.
+    public int getQuantity(String code) {
+        int itemNumber = getItemCodeNumber((code));
+        if (listByCode[itemNumber] == null) {
+            return 0;
+        }
+        return listByCode[itemNumber].size();
+    }
+
+    //EFFECTS: return the number of total quantities in the inventory.
+    public int getTotalQuantity() {
+        return quantity;
+    }
+
+    //EFFECTS: return the list of products specified by the item code.
+    //If there isn't any of those products, return null
+    public ArrayList<Product> getProductList(String itemCode) {
+        ArrayList<Product> list = listByCode[getItemCodeNumber(itemCode)];
+        if (list.size() == 0) {
+            return null;
+        }
+        return list;
+    }
+
+    //EFFECTS: return the product specified by the item code and stock keeping unit(SKU).
+    //If there isn't such a product, return null.
+    public Product getProduct(String itemCode, int sku) {
+        ArrayList<Product> list = listByCode[getItemCodeNumber(itemCode)];
+        for (Product e: list) {
+            if (e.getSku() == sku) {
+                return e;
+            }
+        }
+
+        return null;
+    }
+
+    //EFFECTS: return a list of item codes existing in the inventory.
+    public ArrayList<String> getListOfCodes() {
+        ArrayList<String> codes = new ArrayList<>();
+        for (int i = 0; i < listByCode.length; i++) {
+            ArrayList<Product> products = listByCode[i];
+            if (products.size() != 0) {
+                codes.add(getStringItemCode(i));
+            }
+        }
+        return codes;
+    }
+}
+
---
+++ src/test/ui/StockManagerApplication.java
@@ -0,0 +1,311 @@
+package ui;
+
+import model.Product;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class StockManagerApplication {
+
+
+
+    public static void findLocations(Manager stockManager, Scanner scanner) {
+        System.out.println("enter the item code");
+        String itemCode = scanner.nextLine();
+        ArrayList<String> locationList = stockManager.getLocationListOfProduct(itemCode);
+        System.out.println("The products belonging to the code are stored at: ");
+        for (String e: locationList) {
+            System.out.print(e + " ");
+        }
+        System.out.println();
+    }
+
+    public static void findProduct(Manager stockManager, Scanner scanner) {
+        System.out.println("enter the item code");
+        String itemCode = scanner.nextLine();
+        System.out.println("enter the SKU of the product");
+        int sku = scanner.nextInt();
+        scanner.nextLine();
+        String location = stockManager.getLocationOfProduct(itemCode, sku);
+        if (location != null) {
+            System.out.println("The product: " + itemCode + sku + " is located at " + location);
+        } else {
+            System.out.println("The product doesn't exist in the warehouse");
+        }
+    }
+
+
+
+    public static void checkQuantity(Manager stockManager, Scanner scanner) {
+        System.out.println("enter the item code");
+        String itemCode = scanner.nextLine();
+        int qty = stockManager.countProduct(itemCode);
+        System.out.println("The quantity of " + qty + " of item code" + itemCode + " is stored in the inventory");
+    }
+
+    public static void checkInventory(Manager stockManager) {
+        System.out.println(stockManager.inventoryCheck());
+    }
+
+    public static void printTemporaryList(Manager stockManager) {
+        ArrayList<Object[]> list = stockManager.getTemporaryList();
+        for (Object[] productInfo : list) {
+            Product product = (Product)productInfo[0];
+            String location = (String)productInfo[1];
+            System.out.println(product.getItemCode() + " " + product.getSku());
+            System.out.println("Cost: " + product.getCost());
+            if (product.getBestBeforeDate() != null) {
+                System.out.print(product.getBestBeforeDate());
+            }
+            System.out.println("Location: " + (location.equalsIgnoreCase("T")
+                    ? "Temporary storage space in the inventory" : location));
+        }
+        System.out.println();
+    }
+
+    public static void createLoginAccount(Manager stockManager, Scanner scanner) {
+        String id;
+        System.out.println("enter ID");
+        id = scanner.nextLine();
+        System.out.println("enter PW");
+        String pw = scanner.nextLine();
+        System.out.println("enter your name");
+        String name = scanner.nextLine();
+        System.out.println("enter your birth day in the form: YYYY MM DD");
+        int year = scanner.nextInt();
+        int month = scanner.nextInt();
+        int day = scanner.nextInt();
+        LocalDate birthDay = LocalDate.of(year, month, day);
+        System.out.println("enter your personal code");
+        int personalCode = scanner.nextInt();
+        if (stockManager.createLoginAccount(id, pw, name, birthDay, personalCode)) {
+            System.out.println("Successfully created");
+        } else {
+            System.out.println("Error occurred");
+        }
+        scanner.nextLine();
+    }
+
+    public static void updateInventory(Manager stockManager, Scanner scanner) {
+        System.out.println("Would you add extra info about this update?"
+                + "if yes, enter a line of description. If no, enter n");
+        String description = scanner.nextLine();
+        if (description.equalsIgnoreCase("n")) {
+            stockManager.updateInventory("");
+        } else {
+            stockManager.updateInventory(description);
+        }
+        System.out.println("Successfully updated");
+    }
+
+    public static void removeProduct(Manager stockManager, Scanner scanner) {
+        System.out.println("enter the item code");
+        String itemCode = scanner.nextLine();
+        System.out.println("enter SKU of the product");
+        int sku = scanner.nextInt();
+        scanner.nextLine();
+        if (stockManager.removeProduct(itemCode, sku)) {
+            System.out.println("Successfully removed");
+        } else {
+            System.out.println("Failed removing. the product cannot be found in the list");
+        }
+    }
+
+
+    public static void removeProducts(Manager stockManager, Scanner scanner) {
+        System.out.println("enter the item code");
+        String itemCode = scanner.nextLine();
+        System.out.println("enter quantity");
+        int qty = scanner.nextInt();
+        if (stockManager.removeProducts(itemCode, qty)) {
+            System.out.println("Failed removing. the product cannot be found in the list");
+        }
+    }
+
+    public static void removeProductFromTemporary(Manager stockManager, Scanner scanner) {
+        System.out.println("enter the item code");
+        String itemCode = scanner.nextLine();
+        if (stockManager.removeProductFromTemporaryList(itemCode)) {
+            System.out.println("Successfully removed");
+            return;
+        }
+        System.out.println("Failed removing. the product cannot be found in the list");
+    }
+
+
+    public static void retrievePassword(Manager stockManager, Scanner scanner) {
+        System.out.println("enter id");
+        String id = scanner.nextLine();
+        System.out.println("enter name");
+        String name = scanner.nextLine();
+        System.out.println("enter birthday in YYYY MM DD form");
+        LocalDate birthday = LocalDate.of(scanner.nextInt(), scanner.nextInt(), scanner.nextInt());
+        System.out.println("enter personal code");
+        int personalNum = scanner.nextInt();
+        String retrieved = stockManager.retrievePassword(id, name, birthday, personalNum);
+        if (retrieved != null) {
+            System.out.println(retrieved);
+        } else {
+            System.out.println("No account exists with such information");
+        }
+    }
+
+
+
+
+
+
+
+
+    @SuppressWarnings({"checkstyle:MethodLength", "checkstyle:SuppressWarnings"})
+    public static void createProduct(Manager stockManager, Scanner scanner) {
+        String itemCode;
+        LocalDate bestBeforeDate = null;
+        double cost;
+        System.out.println("enter itemCode");
+        itemCode = scanner.nextLine();
+        System.out.println("Does the product have a best-before date? enter Y/N");
+        if (scanner.nextLine().equalsIgnoreCase(("Y"))) {
+            System.out.println("enter best-before date in the form: YYYY MM DD");
+            bestBeforeDate = LocalDate.of(scanner.nextInt(), scanner.nextInt(), scanner.nextInt());
+        }
+        System.out.println("enter cost");
+        cost = scanner.nextDouble();
+        scanner.nextLine();
+        System.out.println("enter quantity");
+        int qty = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("enter Location in the form: ADD, where A represents an alphabet, D represents a digit"
+                + " if you'd like to put the product in the temporary storage area of the inventory "
+                + "press 'T/t'");
+        String location = scanner.nextLine();
+        for (int i = 0; i < qty; i++) {
+            stockManager.createProduct(itemCode, bestBeforeDate, cost, location);
+        }
+        System.out.println("The product has been successfully created" + '\n' + "Current temporary List: ");
+        printTemporaryList(stockManager);
+        System.out.println("Would you like to add another product? enter Y/N");
+        if (scanner.nextLine().equalsIgnoreCase("Y")) {
+            createProduct(stockManager, scanner);
+        }
+    }
+
+
+    public static void openLedger(Manager stockManager, Scanner scanner) {
+        System.out.println(stockManager.getAccounts());
+        System.out.println("If you'd like to check a particular account more in detail, enter the account code "
+                + "Otherwise, enter q");
+        String option = scanner.nextLine();
+        while (!option.equalsIgnoreCase("q")) {
+            int accountCode = Integer.parseInt(option);
+            System.out.println(stockManager.checkAccount(accountCode));
+            System.out.println("If you'd like to check a particular account more in detail, enter the account code "
+                    + "Otherwise, enter q");
+            option = scanner.nextLine();
+        }
+    }
+
+
+
+    public static void printOptions() {
+        System.out.println("Please select an option:");
+        System.out.println("createAccount: create a new login account");
+        System.out.println("create: create a new product");
+        System.out.println("update: store the newly created items in the storage");
+        System.out.println("removeI: remove a certain number of products from the inventory that is "
+                + "belonging to the specified item code");
+        System.out.println("removeT: remove a product belonging "
+                + "to the specified item code from the temporary storage");
+        System.out.println("findLocations: find locations of products belonging to a specific item code");
+        System.out.println("find: find the location of a particular product with its product code and sku");
+        System.out.println("logout: Log out");
+    }
+
+
+
+
+
+
+
+
+    @SuppressWarnings({"checkstyle:MethodLength", "checkstyle:SuppressWarnings"})
+    public static void main(String[] args) {
+        Manager stockManager = new Manager();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please create a login account first");
+        createLoginAccount(stockManager, scanner);
+        printOptions();
+        String option = scanner.nextLine();
+        boolean login = true;
+        while (!option.equalsIgnoreCase("q")) {
+            if (!login) {
+                System.out.println("Please sign in first");
+                System.out.println("Enter ID");
+                String id = scanner.nextLine();
+                System.out.println("Enter PW");
+                String pw = scanner.nextLine();
+                if (stockManager.adminAccountCheck(id, pw)) {
+                    login = true;
+                } else {
+                    System.out.println("Login failed");
+                }
+            } else {
+                printOptions();
+                option = scanner.nextLine();
+                switch (option) {
+                    case "createAccount":
+                        createLoginAccount(stockManager, scanner);
+                        break;
+                    case "create":
+                        createProduct(stockManager, scanner);
+                        break;
+                    case "update":
+                        updateInventory(stockManager, scanner);
+                        break;
+                    case "removeI":
+                        removeProducts(stockManager, scanner);
+                        break;
+
+                    case "removeProduct":
+                        removeProduct(stockManager, scanner);
+                        break;
+
+                    case "removeT":
+                        removeProductFromTemporary(stockManager, scanner);
+                        break;
+
+                    case "findLocations":
+                        findLocations(stockManager, scanner);
+                        break;
+
+                    case "findProduct":
+                        findProduct(stockManager, scanner);
+                        break;
+
+                    case "checkQuantity":
+                        checkQuantity(stockManager, scanner);
+                        break;
+
+                    case "checkI":
+                        checkInventory(stockManager);
+                        break;
+
+
+                    case "retrievePW":
+                        retrievePassword(stockManager, scanner);
+                        break;
+
+
+                    case "openLedger":
+                        openLedger(stockManager, scanner);
+                        break;
+                    case "logout":
+                        login = false;
+                        break;
+                }
+            }
+        }
+    }
+
+}
