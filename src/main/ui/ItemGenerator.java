924400ae922c448471514557e53c7db706e9a885
JANG <kennethjang34@gmail.com>
2021-11-12 10:41:59 -0800

    CategoryGenerator, Item Generated created

--- src/main/ui/ItemGenerator.java
+++ src/main/ui/ItemGenerator.java
@@ -1,5 +1,6 @@
 package ui;
 
+import javafx.geometry.HorizontalDirection;
 import model.Inventory;
 
 import javax.swing.*;
@@ -19,19 +20,20 @@ public class ItemGenerator extends JPanel {
 
     public ItemGenerator(Inventory inventory) {
         this.inventory = inventory;
-        setPreferredSize(new Dimension(500, 600));
-        add(new JLabel("ID: "));
-        add(idField);
-        add(new JLabel("NAME: "));
-        add(nameField);
-        add(new JLabel("CATEGORY: "));
-        add(categoryField);
-        add(new JLabel("LIST PRICE: "));
-        add(priceField);
-        add(new JLabel("DESCRIPTION: "));
-        add(description);
-        add(new JLabel("NOTE: "));
-        add(note);
+        JPanel fieldPanel = new JPanel();
+        fieldPanel.setLayout(new GridLayout(6, 2));
+        fieldPanel.add(new JLabel("ID: "));
+        fieldPanel.add(idField);
+        fieldPanel.add(new JLabel("NAME: "));
+        fieldPanel.add(nameField);
+        fieldPanel.add(new JLabel("CATEGORY: "));
+        fieldPanel.add(categoryField);
+        fieldPanel.add(new JLabel("LIST PRICE: "));
+        fieldPanel.add(priceField);
+        fieldPanel.add(new JLabel("DESCRIPTION: "));
+        fieldPanel.add(description);
+        fieldPanel.add(new JLabel("NOTE: "));
+        fieldPanel.add(note);
         button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -50,6 +52,14 @@ public class ItemGenerator extends JPanel {
                 }
             }
         });
-        add(button);
+        setLayout(new GridBagLayout());
+        GridBagConstraints gc = new GridBagConstraints();
+        gc.fill = GridBagConstraints.HORIZONTAL;
+        gc.gridx = 0;
+        gc.gridy = 0;
+        add(fieldPanel, gc);
+        gc.gridx = 1;
+        gc.gridy = 1;
+        add(button, gc);
     }
 }
